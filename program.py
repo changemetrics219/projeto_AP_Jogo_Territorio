3e2fbdba4cb86e7e5f3b8bcd9116f015d1ec72e4
changemetrics219 <51268252+changemetrics219@users.noreply.github.com>
2025-01-04 23:27:56 +0000

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,10 @@
 from view import *
 if __name__ == "__main__":
     main()   
+    #linha 33 controller
+    #pensar em uma forma de adicionar as peças a cada jogador e no fim remover as peças
+    
+    #movimentação 
+    #peças aparecerem
+    #verificar regras de colocar as peças
+    
