9e380534c13258505223e2312d9d028b460032d7
changemetrics219 <51268252+changemetrics219@users.noreply.github.com>
2025-01-04 23:28:38 +0000

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,12 +1,12 @@
-#def(function):
-    #aqui vão ficar as funçoes
 import random
 import json
 
 def registar_jogador(matriz, nome):
     if verificar_jogador(matriz, nome) == False:
-        j = {"Nome": nome, "Pontuação":0}
+        j = {'ID':len(matriz) + 1,"Nome": nome, "Pontuação":0}
         matriz.append(j)
+    else:
+        return True
 def verificar_jogador(matriz, nome):
     for jogador in matriz:
          if nome == jogador["Nome"]:
@@ -23,10 +23,26 @@ def visualizar_pontuacao(matriz):
     for jogador in matriz:
         print(f"{jogador['Nome']}: {jogador['Pontuação']}")
 
-
-def adicionar_bonus(matriz, tabuleiro): # isto não esta feito
-    aleatorio = random.randint(0, len(tabuleiro) - 1)
-
+def associar_peca_jogador(ij_n_jogador, jogadores):#-------------------
+    cores = ["azul","verde","amarelo","vermelho"] 
+    for i in range(0, ij_n_jogador):
+        print(f"Escolha uma cor{cores}")
+        
+        p = input (f"{jogadores[i]['Nome']}, qual a peça?")
+        if p == "azul":
+s            jogadores.append({"Peça":"🟦", 'NUMERO_PEÇAS':21})
+            p = "🟦"
+        elif p == "verde":
+            p = "🟩"
+        elif p == "amarelo":
+            p = "🟨"
+        elif p == "vermelho":
+            p = "🟥"
+        
+def dessassociar_peca_jogador(ij_n_jogador, jogadores):
+    jogadores.pop("Peça")
+    
+    
 import os
 def criar_tabuleiro():
     cols = int(input("Insira o tamanho do tabuleiro: "))
@@ -39,10 +55,10 @@ def criar_tabuleiro():
        print(''.join(line))
     while True:
         lin_alterar = int(input("Qual linha deseja alterar?: "))
-        col_alterar = int(input("Qual coluna deseja alterar?: "))
+        col_alterar = int(input("Qual coluna deseja alterar?: "))  
         lin_alterar -= 1   #apagar caso se queira começar com coordenadas 0,0 
         col_alterar -= 1
-        
+        #associar o jogador á peça e vice-versa
         var = int(input("qual peça?"))
         match var:
             case 1:
@@ -61,53 +77,18 @@ def criar_tabuleiro():
 def atualizar_tabuleiro(linha, coluna, jogada, tabuleiro): # proxima coisa a mudar----------------------------
     if tabuleiro[linha][coluna] == '_': tabuleiro[linha][coluna] = jogada
     else: print("Posição já ocupada!")
-"""
-def num_jogadores(jogadores):# transformar isto varias funçoes
-    # for j in jogadores:
-         #if len(jogadores) == 2:
-
-    for j in len(jogadores):
-        if j == 2:
-            nome_1 = input("Introduza o nome do jogador 1: ").upper
-            nome_2 = input("Introduza o nome do jogador 2: ").upper
-            if verificar_jogador(jogadores, nome_1) and verificar_jogador(jogadores, nome_2):
-                print(f"\nJogadores: {jogadores} \n {nome_1} vs {nome_2}")
-            else:
-                print(f"\n Jogadores inválidos!\n Os jogadores {nome_1} ou {nome_2} não estão registados.")
-                break
-                    
-        elif j == 3:
-            nome_1 = input("Introduza o nome do jogador 1: ").upper
-            nome_2 = input("Introduza o nome do jogador 2: ").upper
-            nome_3 = input("Introduza o nome do jogador 3: ").upper
-            if verificar_jogador(jogadores, nome_1) and verificar_jogador(jogadores, nome_2) and verificar_jogador(jogadores, nome_3):
-                print(f"\nJogadores: {jogadores} \n {nome_1} vs {nome_2} vs {nome_3} ")
-            else:
-                print(f"\n Jogadores inválidos!\n Os jogadores {nome_1} ou {nome_2} ou {nome_3} não estão registados.")
-                break
-
-        elif j == 4:
-            nome_1 = input("Introduza o nome do jogador 1: ").upper
-            nome_2 = input("Introduza o nome do jogador 2: ").upper
-            nome_3 = input("Introduza o nome do jogador 3: ").upper
-            nome_4 = input("Introduza o nome do jogador 4: ").upper
-            if verificar_jogador(jogadores, nome_1) and verificar_jogador(jogadores, nome_2) and verificar_jogador(jogadores, nome_3) and verificar_jogador(jogadores, nome_4):
-                print(f"\nJogadores: {jogadores} \n {nome_1} vs {nome_2} vs {nome_3} vs {nome_4} ")
-            else:
-                print(f"\n Jogadores inválidos!\n Os jogadores {nome_1} ou {nome_2} ou {nome_3} ou {nome_4} não estão registados.")
-                break
-        else:
-            print("Introduziu um número de jogadores inválido.")
-            break
-"""
+    
 def ler_ficheiro_json(nome_ficheiro):
     with open(nome_ficheiro) as f:
         data = json.load(f)
     return data 
+
+def adicionar_bonus(matriz, tabuleiro): # isto não esta feito
+    aleatorio = random.randint(0, len(tabuleiro) - 1)
 
 def escrever_ficheiro_json(nome_ficheiro, d):
     json_string = json.dumps(d)
     json_file = open(nome_ficheiro, "w")
     json_file.write(json_string)
     json_file.close()
-    #---------------------------------------------------------------------------------------------------------        
+    #---------------------------------------------------------------------------------------------------------              
