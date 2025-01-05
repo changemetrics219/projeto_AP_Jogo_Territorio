ebdc3f57047db74566336b6955127fac01a5b928
João Luz <51268252+changemetrics219@users.noreply.github.com>
2025-01-05 02:47:22 +0000

    Update program.py

--- program.py
+++ program.py
@@ -8,3 +8,9 @@ if __name__ == "__main__":
     #peças aparecerem
     #verificar regras de colocar as peças
     
+    
+    #------------ regras para inicio de jogo--------
+    #0º verificar o numero de jogadores, quando >2 avança
+    #1º associar peças aos jogadores com funçao  associar_peca_jogador
+    #2º tabuleiro
+    #3º dessociar peças aos jogadores com funçao dessassociar_peca_jogador 
